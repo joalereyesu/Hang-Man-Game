db7d44aa49979a98c9b05d0d9ff02c76a8e639a5
marcelamelgar <61280126+marcelamelgar@users.noreply.github.com>
2020-05-16 22:03:02 -0600

    halfway

--- ContactBook.py
+++ ContactBook.py
@@ -1,4 +1,6 @@
-import os, sys, yaml, requests, json
+import os, sys, yaml, requests, json, validators, emoji, time
+
+exit = False
 
 contacts = {}
 
@@ -7,60 +9,49 @@ open('contacts.json', 'wb').write(getFile.content)
 with open('contacts.json', 'r') as JSON:
     contacts = json.load(JSON)
 
-print(contacts)
-
 def NewContact():
     nombre = input("Ingrese nombre del nuevo contacto\n")
-    while(not(all(c.isalpha() or c == ' ' for c in nombre))):
-        nombre = input("es un nombre invalido\nPor favor ingrese el NOMBRE y APELLIDO del nuevo contacto:")
-    else:
-        print(f'nuevo contacto: {nombre}')
+    for c in nombre:
+        if c.isalpha():
+                break
+        else:
+            print("ingresa unicamente letras para el nombre y el apellido")
 
     phone = input("Ingrese telefono del nuevo contacto\n")
+    for chars in phone:
+        if chars.isnumeric():
+            break
+        else:
+            print(f"El numero {phone} contiene una letra. Ingreselo de nuevo.")
+        
     email = input("Ingrese el correo del nuevo contacto\n")
+    validate = validators.email(email)
+    if validate == False:
+        print('has ingresado el correo', email + 'y este no es valido\nporfavor ingresa un correo como este:\nusuario@ejemplo.com')
+        sys.exit(1)
+    else:
+        print('el correo',email + ' es valido')
+
     company = input("Ingrese la compañia o el lugar en donde trabaja del nuevo contacto\n")
     extra = input("Ingrese informacion extra del nuevo contacto\n")
-    contacts[nombre] = {phone, email, company, extra}
-
-def EditContact():
-    nombre = input('ingrese nombre y apellido del contacto que desea editar\n')
-    input_edition = int(input('1. Editar Telefono\n2. Editar Correo\n3. Editar Empresa\n4. Editar Extra\n 5. Regresar a Menu\n'))
-    leave = False
-    while not leave:
-        if input_edition ==1:
-            phone = input('ingrese nuevo telefono del contacto\n')
-            continue
-        if input_edition == 2:
-            email = str(input("Ingrese nuevo correo del contacto\n"))
-            continue                                                            #aqui hay que ver que pedo, por que no funcio
-        if input_edition == 3:
-            company = str(input("ingrese nueva empresa del contacto\n"))
-            continue
-        if input_edition == 4:
-            extra = str(input("ingrese nueva informacion extra del contacto"))
-        elif input_edition ==5:
-            leave = True
-    exists = nombre in contacts
-    if exists:
-        contacts[nombre] = phone
-    else:
-        print("El contacto no existe, intentelo de nuevo\n")
+    contacts[nombre] = {phone, email, company, extra }
+    print(contacts)
 
 def SearchContact():
     nombre = input("Ingrese nombre del contacto que quiere buscar\n")
     exists = nombre in contacts
 
-    if exists:
-        print(nombre + " " + contacts[nombre])
+    while not exists:
+        input("El contacto no existe, intentelo de nuevo\n")
     else:
-        print("El contacto no existe, intentelo de nuevo\n")
+        print(nombre + " " + contacts[nombre])
 
 def EliminateContact():
-    nombre = input("Ingrese nombre del contacto que quiere eliminar\n")
-    exists = nombre in contacts
+    input_nombre = input("Ingrese nombre del contacto que quiere eliminar\n")
+    existe = input_nombre in contacts
 
-    if exists:
-        del contacts[nombre]
+    if existe:
+        del contacts[input_nombre]
         print("Contacto eliminado con exito!\n")
     else:
         print("El contacto no existe, intentelo de nuevo\n")
@@ -71,24 +62,85 @@ def SeeContacts():
 
 def csvContactos ():
     with open('Your_Contact_List.yaml', 'w') as f:
-        new_doc = yaml.dump(contacts, f)
+        yaml.dump(contacts, f)
         print(yaml.dump(contacts))
         print('has copiado los datos al archivo llamado Your_Contact_List')
 
+def CallContact ():
+    start = 0
+    llamando = input("ingrese el nombre del contacto que desea llamar:")
+    exists = llamando in contacts
+    while not exists:
+        input("El contacto no existe, intentelo de nuevo\n")
+    else:
+        print(emoji.emojize(':telephone_receiver:') + f' estas llamando a "{llamando}"...')
+    while start < 3:
+         m, s = divmod(start, 60)
+         h, m = divmod(m, 60)
+         str(h).zfill(2) + ":"  + str(s).zfill(2)
+         time.sleep(1)
+         start += 1
+         if (start == 3):
+             os.system('clear')
+             break
+
+def MessageContact():
+    start = 0
+    sendmensaje = input("ingrese el nombre del contacto al que desea mandar un mensaje:")
+    exists = sendmensaje in contacts
+    while not exists:
+        input("El contacto no existe, intentelo de nuevo\n")
+    else:
+        mensaje = input("mensaje:")
+        print(emoji.emojize(':outbox_tray:') + f'to: {sendmensaje}\n>> {mensaje}\n')
+    while start < 5:
+         m, s = divmod(start, 60)
+         h, m = divmod(m, 60)
+         str(h).zfill(2) + ":"  + str(s).zfill(2)
+         time.sleep(1)
+         start += 1
+         if (start == 3):
+             os.system('clear')
+             break
+
+def EmailContact():
+    start = 0
+    sendemail = input("ingrese el nombre del contacto al que desea mandar un correo:")
+    exists = sendemail in contacts
+    while not exists:
+        input("El contacto no existe, intentelo de nuevo\n")
+    else:
+        subject = input("subject:")
+        mensaje2 = input("mensaje:")
+    print(emoji.emojize(':e-mail:') + f'\nsending email to: {sendemail}\n- {subject}\n>>{mensaje2}\n')
+    while start < 6:
+         m, s = divmod(start, 60)
+         h, m = divmod(m, 60)
+         str(h).zfill(2) + ":"  + str(s).zfill(2)
+         time.sleep(1)
+         start += 1
+         if (start == 3):
+             os.system('clear')
+             break
+
 while not exit:
 
-    input_menu = int(input(" 1. Crear Contacto \n 2. Editar Contacto\n 3. Buscar Contacto\n 4. Eliminar Contacto\n 5.Ver Contactos\n 6. Agregar Contactos a Archivo\n 7. Salir\n"))
+    input_menu = int(input(" 1. Crear Contacto \n 2. Buscar Contacto\n 3. Ver Contactos\n 4. Eliminar Contacto\n 5. Llamar Contacto\n 6. Enviar Mensaje a Contacto\n 7. Enviar Correo a Contacto\n 8. Agregar Contactos a Archivo\n 9. Salir\n"))
     if input_menu == 1:
         NewContact()
     if input_menu == 2:
-        EditContact()
-    if input_menu == 3:
         SearchContact()
+    if input_menu == 3:
+        SeeContacts()
     if input_menu == 4:
         EliminateContact()
     if input_menu == 5:
-        SeeContacts()
+        CallContact()
     if input_menu == 6:
+        MessageContact()
+    if input_menu == 7:
+        EmailContact()
+    if input_menu == 8:
         csvContactos ()
-    elif input_menu == 7:
+    if input_menu == 9:
         exit = True
